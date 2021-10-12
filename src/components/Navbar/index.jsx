42ed220c94dc83cfe8eb26a64d4810640df2213f
gustave ploix <gustaveploix.pro@gmail.com>
2021-10-12 12:56:56 +0200

    added the toggling + and - in the more link

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,65 +1,80 @@
+import { useState } from "react";
+
 import "./style.scss";
+
+import DropdownNavbar from "../DropdownNavbar";
 
 import logo from "../../images/logo.svg";
 
 export default function Navbar() {
+
+    const [hide, setHide] = useState(true);
+
+
   return (
     <nav>
-      <div className="left">
-        <img className="logo-svg" src={logo} alt="" />
-        <p>
-          <a className="nav-link" href="#">
-            Live
-          </a>
-        </p>
-        <p>
-          <a className="nav-link" href="#">
-            Push
-          </a>
-        </p>
-        <p>
-          <a className="nav-link" href="#">
-            Link
-          </a>
-        </p>
-        <p>
-          <a className="nav-link" href="#">
-            Shop
-          </a>
-        </p>
-        <p>
-          <a className="nav-link" href="#">
-            Packs
-          </a>
-        </p>
-        <p>
-          <a className="nav-link" href="#">
-            Help
-          </a>
-        </p>
-        <p>
-          <a className="nav-link" href="#">
-            More +
-          </a>
-        </p>
-      </div>
+      <div className="navbar-row">
+        <div className="left">
+          <img className="logo-svg" src={logo} alt="" />
+          <p>
+            <a className="nav-link" href="#">
+              Live
+            </a>
+          </p>
+          <p>
+            <a className="nav-link" href="#">
+              Push
+            </a>
+          </p>
+          <p>
+            <a className="nav-link" href="#">
+              Link
+            </a>
+          </p>
+          <p>
+            <a className="nav-link" href="#">
+              Shop
+            </a>
+          </p>
+          <p>
+            <a className="nav-link" href="#">
+              Packs
+            </a>
+          </p>
+          <p>
+            <a className="nav-link" href="#">
+              Help
+            </a>
+          </p>
+          <p>
+            <a className="nav-link" href="#"
+                onClick={() => setHide(!hide)}
+            >
+              More {hide ? "+" : "-"}
+            </a>
+          </p>
+        </div>
 
-      <div className="right">
-        <p>
-          <a className="nav-link blue-link" href="#">
-            Try Live for free
-          </a>
-        </p>
-        <p>
-          <a className="nav-link smaller-link" href="#">
-            Account
-          </a>
-        </p>
-        <p>
-          <a className="nav-link smaller-link" href="#">
-            Log Out
-          </a>
-        </p>
+        <div className="right">
+          <p>
+            <a className="nav-link blue-link" href="#">
+              Try Live for free
+            </a>
+          </p>
+          <p>
+            <a className="nav-link smaller-link" href="#">
+              Account
+            </a>
+          </p>
+          <p>
+            <a className="nav-link smaller-link" href="#">
+              Log Out
+            </a>
+          </p>
+        </div>
+      </div>
+      <div className={hide ? "dropdown-row-closed" : "dropdown-row-open"}>
+        <DropdownNavbar hide={hide} />
       </div>
     </nav>
   );
