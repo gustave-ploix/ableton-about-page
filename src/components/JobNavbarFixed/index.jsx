f3239a8058a71eb31e28309c56413e1bb3affc79
gustave ploix <gustaveploix.pro@gmail.com>
2021-10-12 13:48:26 +0200

    created a simple nav menu to the third part of the navbar

---
+++ src/components/JobNavbarFixed/index.jsx
@@ -0,0 +1,12 @@
+import './style.scss';
+
+export default function JobNavbarFixed() {
+
+
+    return (
+        <div className="jobRow">
+            <p><a className="active-link" href="#0">About</a></p>
+            <p><a href="#0">Jobs</a></p>
+        </div>
+    )
+}
