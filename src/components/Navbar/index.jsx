c9ca7e390cb466c585669710b055b3ffc63a99b2
gustave ploix <gustaveploix.pro@gmail.com>
2021-10-12 13:49:26 +0200

    made some changes about the nav to have the job menu after the separation

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,79 +3,80 @@ import { useState } from "react";
 import "./style.scss";
 
 import DropdownNavbar from "../DropdownNavbar";
+import JobNavbarFixed from "../JobNavbarFixed";
 
 import logo from "../../images/logo.svg";
 
 export default function Navbar() {
-
-    const [hide, setHide] = useState(true);
-
+  const [hide, setHide] = useState(true);
 
   return (
     <nav>
-      <div className="navbar-row">
-        <div className="left">
-          <img className="logo-svg" src={logo} alt="" />
-          <p>
-            <a className="nav-link" href="#">
-              Live
-            </a>
-          </p>
-          <p>
-            <a className="nav-link" href="#">
-              Push
-            </a>
-          </p>
-          <p>
-            <a className="nav-link" href="#">
-              Link
-            </a>
-          </p>
-          <p>
-            <a className="nav-link" href="#">
-              Shop
-            </a>
-          </p>
-          <p>
-            <a className="nav-link" href="#">
-              Packs
-            </a>
-          </p>
-          <p>
-            <a className="nav-link" href="#">
-              Help
-            </a>
-          </p>
-          <p>
-            <a className="nav-link" href="#"
-                onClick={() => setHide(!hide)}
-            >
-              More {hide ? "+" : "-"}
-            </a>
-          </p>
-        </div>
+      <div className="navbar">
+        <div className="navbar-row">
+          <div className="left">
+            <img className="logo-svg" src={logo} alt="" />
+            <p>
+              <a className="nav-link" href="#0">
+                Live
+              </a>
+            </p>
+            <p>
+              <a className="nav-link" href="#0">
+                Push
+              </a>
+            </p>
+            <p>
+              <a className="nav-link" href="#0">
+                Link
+              </a>
+            </p>
+            <p>
+              <a className="nav-link" href="#0">
+                Shop
+              </a>
+            </p>
+            <p>
+              <a className="nav-link" href="#0">
+                Packs
+              </a>
+            </p>
+            <p>
+              <a className="nav-link" href="#0">
+                Help
+              </a>
+            </p>
+            <p>
+              <a className="nav-link" href="#0" onClick={() => setHide(!hide)}>
+                More {hide ? "+" : "-"}
+              </a>
+            </p>
+          </div>
 
-        <div className="right">
-          <p>
-            <a className="nav-link blue-link" href="#">
-              Try Live for free
-            </a>
-          </p>
-          <p>
-            <a className="nav-link smaller-link" href="#">
-              Account
-            </a>
-          </p>
-          <p>
-            <a className="nav-link smaller-link" href="#">
-              Log Out
-            </a>
-          </p>
+          <div className="right">
+            <p>
+              <a className="nav-link blue-link" href="#0">
+                Try Live for free
+              </a>
+            </p>
+            <p>
+              <a className="nav-link smaller-link" href="#0">
+                Account
+              </a>
+            </p>
+            <p>
+              <a className="nav-link smaller-link" href="#0">
+                Log Out
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className={hide ? "dropdown-row-closed" : "dropdown-row-open"}>
+          <DropdownNavbar hide={hide} />
         </div>
       </div>
-      <div className={hide ? "dropdown-row-closed" : "dropdown-row-open"}>
-        <DropdownNavbar hide={hide} />
-      </div>
+
+      <JobNavbarFixed />
     </nav>
   );
 }
