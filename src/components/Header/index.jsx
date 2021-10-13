065bbe3c2e06f11585973a2a37c7ee96d80c4d69
gustave ploix <gustaveploix.pro@gmail.com>
2021-10-13 11:05:32 +0200

    created and finished the header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,15 @@
+import './style.scss';
 
+import background from '../../images/header.jpg'
 
 export default function Header() {
 
 
     return (
-        <header>
-            
+        <header style={{backgroundImage: `url(${background})`}}>
+            <h1 className="header__title">
+                Ableton
+            </h1>
         </header>
     )
 }
