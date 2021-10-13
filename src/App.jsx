c96186ebf0a57be31b6369b8a01bdabfb0ca76a3
gustave ploix <gustaveploix.pro@gmail.com>
2021-10-13 11:06:06 +0200

    added the presentation section to the App file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Navbar from "./components/Navbar";
 import Header from './components/Header';
+import PresentationSection from "./components/PresentationSection";
 
 import './style.scss';
 
@@ -10,6 +11,7 @@ export default function App() {
         <>
         <Navbar />
         <Header />
+        <PresentationSection />
         </>
     );
 };
