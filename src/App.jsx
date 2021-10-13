f810edfb8d0de77206de1b53f7a9ec9f12369c4b
gustave ploix <gustaveploix.pro@gmail.com>
2021-10-13 10:53:57 +0200

    imported the header to the main App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 import Navbar from "./components/Navbar";
+import Header from './components/Header';
 
 import './style.scss';
 
 export default function App() {
     return (
+        <>
         <Navbar />
+        <Header />
+        </>
     );
 };
