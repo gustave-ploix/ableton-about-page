b6f78f3731be0c479bc4a49b49f9b4dd6baab556
gustave ploix <gustaveploix.pro@gmail.com>
2021-10-13 11:32:21 +0200

    stylized the differents elements in the presentation section

--- src/components/PresentationSection/index.jsx
+++ src/components/PresentationSection/index.jsx
@@ -1,12 +1,18 @@
-import './style.scss';
-
+import "./style.scss";
 
 export default function PresentationSection() {
-
-
-    return (
-        <section>
-
-        </section>
-    )
+  return (
+    <section className="presentation-section">
+      <p className="presentation__title">
+        We make <a href="#0">Live</a>, <a href="#0">Push</a> and <a href="#0">Link</a> — unique software and hardware for music
+        creation and performance. With these products, our community of users
+        creates amazing things.
+      </p>
+      <p className="presentation__text">
+        Ableton was founded in 1999 and released the first version of Live in
+        2001. Our products are used by a community of dedicated musicians, sound
+        designers, and artists from across the world.
+      </p>
+    </section>
+  );
 }
