1c5053aa64d52294f5bdd65db1a9c35174a4c2f7
gustave ploix <gustaveploix.pro@gmail.com>
2021-10-14 12:32:30 +0200

    added the differents items in the App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+import {text} from './services/text';
+
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import PresentationSection from "./components/PresentationSection";
 import ImagesSection from "./components/ImagesSection";
+import VideoPlayer from "./components/VideoPlayer";
+
+import TextSection from './components/TextSection';
 
 import "./style.scss";
 
@@ -14,6 +19,9 @@ export default function App() {
       <Header />
       <PresentationSection />
       <ImagesSection />
+      <TextSection title={text[1].title} text={text[1].subtitle} />
+      <VideoPlayer />
+      <TextSection title={text[2].title} text={text[2].subtitle} />
     </>
   );
 }
