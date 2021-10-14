500500eb72f5fe5c3434857dd56e00b35f65eb02
gustave ploix <gustaveploix.pro@gmail.com>
2021-10-14 12:31:29 +0200

    created and styled a template for all the texts this one is à part because it has some links

--- src/components/PresentationSection/index.jsx
+++ src/components/PresentationSection/index.jsx
@@ -2,13 +2,13 @@ import "./style.scss";
 
 export default function PresentationSection() {
   return (
-    <section className="presentation-section">
-      <p className="presentation__title">
+    <section className="text-section">
+      <p className="text-section__title">
         We make <a href="#0">Live</a>, <a href="#0">Push</a> and <a href="#0">Link</a> — unique software and hardware for music
         creation and performance. With these products, our community of users
         creates amazing things.
       </p>
-      <p className="presentation__text">
+      <p className="text-section__text">
         Ableton was founded in 1999 and released the first version of Live in
         2001. Our products are used by a community of dedicated musicians, sound
         designers, and artists from across the world.
